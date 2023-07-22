0b55cd66f03b7b74a38c51384ca838f966bd96d3
Mis25786 <111245815+Mis25786@users.noreply.github.com>
2023-07-22 18:59:41 +0200

    add_remove-pet_advert

--- controllers/pets/removeAdvert.js
+++ controllers/pets/removeAdvert.js
@@ -1,7 +1,17 @@
 const catchAsyncWrapper = require("../../helpers/catchAsyncWrapper");
+const { CustomError } = require("../../helpers");
+const { Pet } = require("../../models/pet");
 
 const removeAdvert = catchAsyncWrapper(async (req, res) => {
-  //* add the code
+  const { petId } = req.params;
+
+  const result = await Pet.findByIdAndRemove(petId);
+
+  if (!result) {
+    throw CustomError(404, "Not found");
+  }
+
+  res.json({ message: "Announcement deleted" });
 });
 
 module.exports = removeAdvert;
