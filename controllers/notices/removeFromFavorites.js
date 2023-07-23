74ac16b7661c377f48016fb1880a0fb95ac14369
Hanna Ponomarenko <hannaponomarenko@ukr.net>
2023-07-23 22:35:16 +0300

    fix bag

--- controllers/notices/removeFromFavorites.js
+++ controllers/notices/removeFromFavorites.js
@@ -1,11 +1,11 @@
 const { httpError } = require("../../helpers");
-const catchAsyncWrapper = require("../../helpers/");
+const { catchAsyncWrapper } = require("../../helpers/");
 
 const User = require("../../models/user");
 
 const removeFromFavorite = catchAsyncWrapper(async (req, res) => {
   const user = req.user;
-  const {noticeId} = req.params;
+  const { noticeId } = req.params;
 
   if (!user.favorite.includes(noticeId)) {
     throw httpError(404, "Not found");
@@ -14,6 +14,7 @@ const removeFromFavorite = catchAsyncWrapper(async (req, res) => {
   await User.findByIdAndUpdate(user._id, {
     $pull: { favorite: noticeId },
   });
+
   res.status(204).json({
     message: "Pet delete success",
   });
