d0a71f6842bd6c142d9323262015d7506dd06eae
VladZhyrnyi <vladzhyrnyi@ukr.net>
2023-07-29 11:04:03 +0300

    fix getNotices, add Pagination

--- controllers/notices/getNotices.js
+++ controllers/notices/getNotices.js
@@ -1,32 +1,39 @@
-const { httpError, catchAsyncWrapper } = require("../../helpers");
+const { catchAsyncWrapper } = require("../../helpers");
 const Notices = require("../../models/notice");
 
-const getNotices = catchAsyncWrapper(async (req, res, next) => {
-    const { searchquery, category } = req.body;
-    if (!searchquery && !category) {
-        throw httpError(400, "Missing field`s");
-    }
 
-   let result = []
+
+const getNotices = catchAsyncWrapper(async (req, res) => {
+    const { query='', category = 'sell', page = 1, perPage = 9 } = req.query;
+
+    const limit = perPage;
+    const skip = (page - 1) * limit;
+
+    const result = await Notices.find({
+      category,
+      title: { $regex: `${query}`, $options: "i" },
+    }, '', {skip, limit});
+
+    // let result = []
       
-    if (searchquery && !category) {
-        result = await Notices.find({
-          title: { $regex: `${searchquery}`, $options: "i" },
-        });
-    }    
+    // if (query && !category) {
+    //     result = await Notices.find({
+    //       title: { $regex: `${query}`, $options: "i" },
+    //     });
+    // }    
     
-    if (!searchquery && category) {
-        result = await Notices.find({
-          category: { $regex: `${category}`, $options: "i" },
-        });
-    }
+    // if (!query && category) {
+    //     result = await Notices.find({
+    //       category: { $regex: `${category}`, $options: "i" },
+    //     });
+    // }
     
-    if (searchquery && category) {
-        result = await Notices.find({
-          category: { $regex: `${category}`, $options: "i" },
-          title: { $regex: `${searchquery}`, $options: "i" },
-        });
-    }
+    // if (query && category) {
+    //     result = await Notices.find({
+    //       category: { $regex: `${category}`, $options: "i" },
+    //       title: { $regex: `${query}`, $options: "i" },
+    //     });
+    // }
     
   res.status(200).json(result);
 });
