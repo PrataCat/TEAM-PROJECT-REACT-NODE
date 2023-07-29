f74d2644a3beca20283d95758908b7f2f5f4a1c2
Dmitriy Kvas <thelostsoul88@MacBook-Pro-Dmitriy.local>
2023-07-29 14:31:10 +0300

    update: addPet

--- controllers/pets/addPet.js
+++ controllers/pets/addPet.js
@@ -5,14 +5,14 @@ const { addSchema } = require("../../schemas");
 
 const addPet = catchAsyncWrapper(async (req, res) => {
   const { error } = addSchema.validate(req.body);
-  // const file = req.file.path;
+  const file = req.file.path;
   const { _id: owner } = req.user;
 
   if (error) {
-    throw httpError(400, "Bad request");
+    throw httpError(400, error.message);
   }
 
-  const result = await Pet.create({ ...req.body, owner });
+  const result = await Pet.create({ ...req.body, owner, file });
 
   res.status(201).json(result);
 });
