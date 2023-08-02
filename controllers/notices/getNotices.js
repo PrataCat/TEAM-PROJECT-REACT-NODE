dac4ee14cd8f5e66f208e6c452c6a2439cd6beb3
Oleksii Korolenko <as_vostok@i.ua>
2023-08-02 10:42:24 +0300

    add change getNotice

--- controllers/notices/getNotices.js
+++ controllers/notices/getNotices.js
@@ -1,40 +1,32 @@
 const { catchAsyncWrapper } = require("../../helpers");
 const Notices = require("../../models/notice");
 
-
-
 const getNotices = catchAsyncWrapper(async (req, res) => {
-    const { query='', category = 'sell', page = 1, perPage = 9 } = req.query;
+  const { query = "", category = "sell", page = 1, perPage = 9 } = req.query;
 
-    const limit = perPage;
-    const skip = (page - 1) * limit;
+  const limit = perPage;
+  const skip = (page - 1) * limit;
 
-    const result = await Notices.find({
+  const noticesAll = await Notices.find({
+    category,
+    title: { $regex: `${query}`, $options: "i" },
+  });
+
+  const notices = await Notices.find(
+    {
       category,
       title: { $regex: `${query}`, $options: "i" },
-    }, '', {skip, limit});
+    },
+    "",
+    { skip, limit }
+  );
+  
+  const totalNotices = noticesAll.length;
 
-    // let result = []
-      
-    // if (query && !category) {
-    //     result = await Notices.find({
-    //       title: { $regex: `${query}`, $options: "i" },
-    //     });
-    // }    
-    
-    // if (!query && category) {
-    //     result = await Notices.find({
-    //       category: { $regex: `${category}`, $options: "i" },
-    //     });
-    // }
-    
-    // if (query && category) {
-    //     result = await Notices.find({
-    //       category: { $regex: `${category}`, $options: "i" },
-    //       title: { $regex: `${query}`, $options: "i" },
-    //     });
-    // }
-    
+  const totalPages = Math.ceil(totalNotices / perPage)
+
+  const result = { totalPages, notices };
+
   res.status(200).json(result);
 });
 module.exports = getNotices;
