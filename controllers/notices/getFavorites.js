6953e322513d3d658eeadc17338bc3a0d8d6df67
Oleksii Korolenko <as_vostok@i.ua>
2023-08-02 12:41:24 +0300

    add pagination on notices/user and notices/favorite

--- controllers/notices/getFavorites.js
+++ controllers/notices/getFavorites.js
@@ -4,10 +4,21 @@ const User = require("../../models/user");
 
 const getFavorites = catchAsyncWrapper(async (req, res) => {
   const { _id } = req.user;
+  const { page = 1, perPage = 9 } = req.query;
 
-  const user = await User.find(_id).populate("favorite");
+  const limit = perPage;
+  const skip = (page - 1) * limit;
 
-  res.json(user[0].favorite);
+  const user = await User.find(_id, "", { skip, limit }).populate("favorite");
+
+  const notices = user[0].favorite;
+
+  const totalNotices = notices.length;
+  const totalPages = Math.ceil(totalNotices / perPage);
+
+  const result = { totalPages, notices };
+
+  res.json(result);
 });
 
 module.exports = getFavorites;
