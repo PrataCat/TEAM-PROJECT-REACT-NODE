a0a18dab129e036c300bc1299bc440acdc7890c5
VladZhyrnyi <vladzhyrnyi@ukr.net>
2023-08-03 23:04:20 +0300

    Update getUserNotices.js
    add search query for user notices

--- controllers/notices/getUserNotices.js
+++ controllers/notices/getUserNotices.js
@@ -3,13 +3,23 @@ const Notice = require("../../models/notice");
 
 const getUserNotices = catchAsyncWrapper(async (req, res) => {
   const { _id: owner } = req.user;
-  const { page = 1, perPage = 9 } = req.query;
+  const { query = "", page = 1, perPage = 9 } = req.query;
 
   const skip = (page - 1) * perPage;
 
-  const notices = await Notice.find({ owner }).skip(skip).limit(perPage);
+  const totalNotices = await Notice.find({
+    owner,
+    title: { $regex: `${query}`, $options: "i" },
+  }).countDocuments();
 
-  const totalNotices = notices.length;
+  console.log(totalNotices)
+
+  const notices = await Notice.find({
+    owner,
+    title: { $regex: `${query}`, $options: "i" },
+  })
+    .skip(skip)
+    .limit(perPage);
 
   const totalPages = Math.ceil(totalNotices / perPage);
 
