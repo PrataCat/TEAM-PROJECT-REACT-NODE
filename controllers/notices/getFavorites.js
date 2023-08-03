77a3bacfa351bcf4f5f0d62350406e0898e8e833
VladZhyrnyi <vladzhyrnyi@ukr.net>
2023-08-03 22:53:17 +0300

    add query search for favorites

--- controllers/notices/getFavorites.js
+++ controllers/notices/getFavorites.js
@@ -5,13 +5,16 @@ const User = require("../../models/user");
 const getFavorites = catchAsyncWrapper(async (req, res) => {
   const { _id } = req.user;
 
-  const { page = 1, perPage = 9 } = req.query;
+  const { query = "", page = 1, perPage = 9 } = req.query;
 
   const limit = perPage;
   const skip = (page - 1) * limit;
 
   const noticesAll = await User.findById(_id)
-    .populate("favorite")
+    .populate({
+      path: "favorite",
+      match: { title: {$regex: `${query}`, $options: "i" }}
+    })
     .select("favorite -_id");
 
   const totalNotices = noticesAll.favorite.length;
@@ -20,6 +23,7 @@ const getFavorites = catchAsyncWrapper(async (req, res) => {
   const { favorite } = await User.findById(_id)
     .populate({
       path: "favorite",
+      match: { title: {$regex: `${query}`, $options: "i" }},
       perDocumentLimit: limit,
       options: {
         skip,
